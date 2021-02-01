ad80b6df9e9cd42c9ce8eb28f44a8e1d7a7cd8f8
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-01 22:49:50 +0530

    Checking if two lines are equal using JAVA_EQUALS method

--- LineComparisonComputationProgram.java
+++ LineComparisonComputationProgram.java
@@ -7,6 +7,7 @@ public class LineComparisonComputationProgram
 	{
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("First line Config");
 		System.out.println("Enter the co-ordinates of First Point x1 y1");
 		int x1 = sc.nextInt();
 		int y1 = sc.nextInt();
@@ -15,10 +16,23 @@ public class LineComparisonComputationProgram
 		int x2 = sc.nextInt();
 		int y2 = sc.nextInt();
 		
-		double length = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+		double length1 = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ) ;
 		
+		System.out.println("Second line Config");
+		System.out.println("Enter the co-ordinates of First Point x3 y3");
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
 		
-		System.out.println("Length of the line is "+length);
+		System.out.println("Enter the co-ordinates of Second Point x4 y4");
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+		
+		double length2 = Math.sqrt( Math.pow((x4-x3),2) + Math.pow((y4-y3),2) );
+		
+		Double len1 = new Double(length1);
+		Double len2 = new Double(length2);
+		
+		System.out.println(len1.equals(len2));
 
 	}
 
