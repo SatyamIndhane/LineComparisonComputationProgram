8427a5b7edb9a6f37aa87d9bc46226a43d435583
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-03 22:24:58 +0530

    Using and clubbing line point equals compareTo method JavaOOP

--- LineComparisonComputationProgram.java
+++ LineComparisonComputationProgram.java
@@ -3,13 +3,27 @@ import java.util.*;
 
 public class LineComparisonComputationProgram 
 {
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		
 		// Line 1 
+		double length1 = Line1();
 		
-		System.out.println("First line Config");
+		// Line 2
+		double length2 = Line2();
+		
+		Double len1 = new Double(length1);
+		Double len2 = new Double(length2);
+		
+		System.out.println("Java compareTo method result = "+len1.compareTo(len2));     
+		System.out.println("Java equals method result = "+len1.equals(len2));      
+
+	}
+	
+	public static double Line1()
+	{
+		System.out.println("First line Configuration");
 		System.out.println("Enter the co-ordinates of First Point x1 y1");
 		int x1 = sc.nextInt();
 		int y1 = sc.nextInt();
@@ -18,13 +32,12 @@ public class LineComparisonComputationProgram
 		int x2 = sc.nextInt();
 		int y2 = sc.nextInt();
 		
-		// Line 1 length Calculation
-		
-		double length1 = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ) ;
-		
-		// Line 2 
-		
-		System.out.println("Second line Config");
+		return Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ) ;
+	}
+	
+	public static double Line2()
+	{
+		System.out.println("Second line Configuration");
 		System.out.println("Enter the co-ordinates of First Point x3 y3");
 		int x3 = sc.nextInt();
 		int y3 = sc.nextInt();
@@ -33,14 +46,7 @@ public class LineComparisonComputationProgram
 		int x4 = sc.nextInt();
 		int y4 = sc.nextInt();
 		
-		// Line 2 length calculation
-		
-		double length2 = Math.sqrt( Math.pow((x4-x3),2) + Math.pow((y4-y3),2) );
-		
-		Double len1 = new Double(length1);
-		Double len2 = new Double(length2);
-		
-		System.out.println(len1.compareTo(len2));
+		return Math.sqrt( Math.pow((x4-x3),2) + Math.pow((y4-y3),2) );
 
 	}
 
